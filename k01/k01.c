7b156b147e8b8b2fd80d7d105beff4b7386a87b5
ooki-takumi <bb35319012@ms.nagasaki-u.ac.jp>
2020-10-15 10:41:35 +0900

    k01の提出です

--- k01/k01.c
+++ k01/k01.c
@@ -64,16 +64,41 @@ int LoadData(City arrayCity[])
 
 int LinearSearch(int key, City arrayCity[], int size)
 {
-    //  ここを実装する
+    int pos;
+    int result;
+    result=-1;
 
+    for(pos;pos<MAX_CITY;pos++){
+        if(key==arrayCity[pos].id){
+        result=pos;
+        }else{
+            result=result;
+        }
+        
+    }
 
+        return result;
 }
 
-int BinarySearch(int key, City arrayCity[], int left, int right)
+int BinarySearch(int key, City arrayCity[], int left, int right )
 {
-    //  ここを実装する
-
-
+    int pos;
+    int result;
+    result=-1;
+    right=MAX_CITY-1;
+    left=0;
+    while(left<=right){
+    pos=left+(right-left)/2;
+    if(key==arrayCity[pos].id){
+        result=pos;
+        break;
+    }else if(key>=arrayCity[pos].id){
+        left=pos+1;
+    }else{
+        right=pos-1;
+    }
+}
+     return result;
 }
 
 
